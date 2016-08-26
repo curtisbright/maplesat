340799c47791a4d68a1907bb44d8e3f02375c16e
Curtis Bright <cbright@gmail.com>
2016-08-26 17:36:22 -0400

    have a separate counter both A and B filtering

--- maplecomsps/MapleCOMSPS/core/Solver.cc
+++ maplecomsps/MapleCOMSPS/core/Solver.cc
@@ -56,6 +56,8 @@ double time4 = 0;
 double time5 = 0;
 int num_complete = 0;
 int filt_success = 0;
+int filt_success_A = 0;
+int filt_success_B = 0;
 
 using namespace Minisat;
 
@@ -1837,6 +1839,7 @@ bool Solver::programmatic_check(vec<Lit>& out_learnt, int&
   { num_complete++;
     if (!hall_check(a_fills,golay_dimension,trials))
     { filt_success++;
+      filt_success_A++;
 #ifdef PRINTCONF
       printf("conflict: filtering theorem for sequence A\n");
 #endif
@@ -1856,6 +1859,7 @@ bool Solver::programmatic_check(vec<Lit>& out_learnt, int&
 #endif
     if (!hall_check(b_fills,golay_dimension,trials))
     { filt_success++;
+      filt_success_B++;
       for (int i = golay_dimension*2;i<no_of_significant_vars; i++)
       {
 	if(assigns[i] == l_False)
@@ -2441,7 +2445,8 @@ lbool Solver::solve_()
     printf("filtering       checks: %d successes, %d total (%.2f), %.2f total time\n", success1, calls1, (float)success1/calls1, time1);
     printf("autocorrelation checks: %d successes, %d total (%.2f), %.2f total time\n", success3, calls3, (float)success3/calls3, time3);
     //printf("algebraic       checks: %d successes, %d total (%.2f), %.2f total time\n", success5, calls5, (float)success5/calls5, time5);
-    printf("filtering               %d successes, %d total (%.2f)\n", filt_success, num_complete, (float)num_complete/filt_success);
+    printf("filtering               %d successes, %d total (%.2f)\n", filt_success, num_complete, (float)filt_success/num_complete);
+    printf("filtering               %d A successes, %d B successes\n", filt_success_A, filt_success_B);
 
     if (status == l_True){
         // Extend & copy model:
